edc38a4745c8d41fe1d0149cff576a361152b3cc
Brazza <jjia@ufl.edu>
2025-10-29 15:07:32 -0400

    Add the price and genre
    Add the price and genre field to offer table

--- backend/UFTicketer/common/models.py
+++ backend/UFTicketer/common/models.py
@@ -20,6 +20,8 @@ class Offers(models.Model):
     buyer = models.ForeignKey('Users', models.DO_NOTHING, related_name='offers_buyer_set', blank=True, null=True)
     description = models.CharField(max_length=1000, blank=True, null=True)
     image_path = models.JSONField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    genre = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         managed = False
